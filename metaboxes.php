0aee60c4b02d8fbe05f052cb3be065c940f8701d
Adam <adam@localhost.localdomain>
2017-01-29 16:45:38 -0600

    added the rating field

--- metaboxes.php
+++ metaboxes.php
@@ -31,11 +31,31 @@ function sonet_register_review_box() {
 function sonet_review_box_content( $post ) {
     // Display code/markup goes here. Don't forget to include nonces!
     	wp_nonce_field( plugin_basename( __FILE__ ), 'sonet_review_box_content_nonce' );
+        // Location
     	echo '<div class="reviewfieldholder">Location: </div><input type="text" '
             . 'id="sonet_review_location" name="sonet_review_location" '
             . 'placeholder="Location" class="admininputfield"value="'
             . get_post_meta($post->ID, "sonet_review_location", true)    
             . '"> <br/>';
+        // Date
+    	echo '<div class="reviewfieldholder">Date: </div><input type="text" '
+            . 'id="sonet_review_date" name="sonet_review_date" '
+            . 'placeholder="mm/dd/yy" class="admininputfield"value="'
+            . get_post_meta($post->ID, "sonet_review_date", true)    
+            . '"> <br/>';
+        // Stars Rating
+        $my_rating = get_post_meta($post->ID, "sonet_review_rating", true);
+        echo '<div class="reviewfieldholder">Rating: </div>'
+          . '<select'
+          . ' id="sonet_review_rating" name="sonet_review_rating" class="admindropdownfield"'
+          . '>';
+        echo "<option value=\"\">(Select One)</option>";
+        $ratings = array("5", "4", "3", "2", "1");
+        foreach($ratings as $rating) {
+            $select = ($rating === $my_rating) ? " selected" : "";
+            echo "<option value=\"$rating\"" . $select . ">$rating</option>";
+        }
+        echo '</select><br>';
 }
 
 add_action( 'save_post', 'sonet_review_meta_save' );
@@ -63,5 +83,7 @@ function sonet_review_meta_save( $post_id ) {
     }
 
     update_post_meta( $post_id, 'sonet_review_location', $_POST['sonet_review_location'] );
+    update_post_meta( $post_id, 'sonet_review_date', $_POST['sonet_review_date'] );
+    update_post_meta( $post_id, 'sonet_review_rating', $_POST['sonet_review_rating'] );
 
 }
--- sonet-reviews.php
+++ sonet-reviews.php
@@ -1,9 +1,12 @@
 <?php
 /*
-Plugin Name: Sonet Reviews
-Description: Reviews plugin using a CPT
-Author: Sonet Digital
-Author URI: http://www.sonetseo.com
+ * Plugin Name: Sonet Reviews
+ * Description: Reviews plugin using a CPT
+ * Author: Sonet Digital
+ * Author URI: http://www.sonetseo.com
+ * Plugin URI: http://www.sonetseo.com/wordpress
+ * Version: 0.1
+ *
 */
 
 include 'metaboxes.php';
@@ -16,6 +19,14 @@ function sonet_reviews_cpt() {
         'labels' => array(
             'name' =>  __( 'Reviews' ),
             'singular_name' =>  __( 'Review' ),
+            'search_items' =>  __( 'Search Review' ),
+            'all_items' => __( 'All Reviews' ),
+            'parent_item' => __( 'Parent Review' ),
+            'parent_item_colon' => __( 'Parent Review:' ),
+            'edit_item' => __( 'Edit Review' ), 
+            'update_item' => __( 'Update Review' ),
+            'add_new_item' => __( 'Add New Review' ),
+            'new_item_name' => __( 'New Review Name' ),
         ),
         'description' => 'Customer reviews which we will be displayed on the website.',
         'menu_position' => 10,
@@ -33,4 +44,27 @@ function sonet_reviews_cpt() {
         'taxonomies'  => array( 'category' )
         )
     );
+}
+
+add_filter('post_updated_messages', 'review_updated_messages');
+function review_updated_messages( $messages ) {
+
+$messages['grid_reviews'] = array(
+0 => '', // Unused. Messages start at index 1.
+1 => sprintf( __('Review updated. <a href="%s">View Review</a>'), esc_url( get_permalink($post_ID) ) ),
+2 => __('Custom field updated.'),
+3 => __('Custom field deleted.'),
+4 => __('Review updated.'),
+/* translators: %s: date and time of the revision */
+5 => isset($_GET['revision']) ? sprintf( __('Review restored to revision from %s'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+6 => sprintf( __('Review published. <a href="%s">View Review</a>'), esc_url( get_permalink($post_ID) ) ),
+7 => __('Contact saved.'),
+8 => sprintf( __('Review submitted. <a target="_blank" href="%s">Preview Review</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+9 => sprintf( __('Review scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview Review</a>'),
+  // translators: Publish box date format, see http://php.net/date
+  date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+10 => sprintf( __('Review draft updated. <a target="_blank" href="%s">Preview Review</a>'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
+);
+
+return $messages;
 }
