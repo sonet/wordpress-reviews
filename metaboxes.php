ecb6476c0a74f2c5e611c5993ae2ac8b59ba430c
Adam <adam@localhost.localdomain>
2017-01-29 11:22:35 -0600

    minor label and id changes

--- metaboxes.php
+++ metaboxes.php
@@ -8,7 +8,7 @@ add_action( 'add_meta_boxes', 'sonet_register_review_box' );
 function sonet_register_review_box() {
     add_meta_box(
       'sonet_review_box',
-      __( 'Review', 'sonet_textdomain' ),
+      __( 'Reviews', 'sonet_textdomain' ),
       'sonet_review_box_content',
       'sonet_review',
       'normal',
@@ -24,5 +24,5 @@ function sonet_register_review_box() {
 function sonet_review_box_content( $post ) {
     // Display code/markup goes here. Don't forget to include nonces!
     	wp_nonce_field( plugin_basename( __FILE__ ), '_sonet_review_box_content_nonce' );
-    	echo '<div class="reviewfieldholder">Location: </div><input type="text" id=sonet_review_location" name=sonet_review_location" placeholder="Location" class="admininputfield"value="'.get_post_meta( $_GET[post], '_grid_review_location', true ).'"> <br/>';
+    	echo '<div class="reviewfieldholder">Location: </div><input type="text" id="sonet_review_location" name="sonet_review_location" placeholder="Location" class="admininputfield"value="'.get_post_meta( $_GET[post], 'sonet_review_location', true ).'"> <br/>';
 }
